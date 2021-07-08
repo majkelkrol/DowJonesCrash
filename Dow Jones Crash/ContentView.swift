bed7acf3a23a6da230123225189fa7da54d3f6d9
Majkel <majkelkrol@icloud.com>
2021-07-08 23:18:01 +0200

    Initial Commit

---
+++ Dow Jones Crash/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dow Jones Crash
+//
+//  Created by Majkel on 08/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dow Jones Crash/Dow_Jones_CrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dow_Jones_CrashApp.swift
+//  Dow Jones Crash
+//
+//  Created by Majkel on 08/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Dow_Jones_CrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
